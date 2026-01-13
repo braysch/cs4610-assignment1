d421bce526e76ba49dac73f79804bf2934818e7b
brayschway <bradanschwanke@gmail.com>
2026-01-12 22:37:31 -0700

    Centered the text and put the index in the top left corner for each pokemon item

--- app/pokemon/page.tsx
+++ app/pokemon/page.tsx
@@ -13,7 +13,7 @@ export default async function PokemonPage() {
       <ul className='grid grid-cols-5 gap-y-4 p-8'>
         {pokemon.results.map((poke, index) => (
           <Link key={poke.name} href={`/pokemon/${poke.name}`}><li className='flex items-center justify-center m-2 bg-gray-900 rounded-xl hover:scale-[110%] transition-all duration-100 hover:bg-gray-700 h-full group' key={poke.name}>
-            <div className="relative w-full">
+            <div className="relative w-full h-full flex justify-center">
             <div className="flex gap-x-2 p-2 justify-center items-center text-center">
                 {poke.name.charAt(0).toUpperCase() + poke.name.slice(1)}
                 <div className="group-hover:rotate-[10deg] transition-all duration-300"><PokemonSpriteImage poke={poke} /></div>
